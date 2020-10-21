37f11bdce2b917d4da5edfa4611d0f9801abf08d
maisyyazid <maisyyazid@student.telkomuniveristy.ac.id>
2020-10-21 14:10:19 +0700

    ada yang harus diganti

--- src/com/latihan/Biner.java
+++ src/com/latihan/Biner.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Biner {
     public static void main(String[] args) {
-        int desimal, biner;
+        int desimal, biner, awal;
         StringBuilder bin = new StringBuilder();
 
         Scanner in = new Scanner(System.in);
 
         System.out.print("masukan desimal: ");
         desimal = in.nextInt();
+        awal = desimal;
 
         while (desimal>0) {
             biner = desimal % 2;
             desimal = desimal / 2;
             bin.insert(0, biner);
         }
-        System.out.println(bin);
+        System.out.println(awal + " binernya adalah: " + bin);
     }
 }
