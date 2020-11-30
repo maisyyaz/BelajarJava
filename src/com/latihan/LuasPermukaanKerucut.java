475a008011fd74df2a450ea53d49bac2be598f83
maisyyazid <ma39isy@gmail.com>
2020-11-30 20:33:14 +0700

    Tugas 13 IMA

--- src/com/latihan/LuasPermukaanKerucut.java
+++ src/com/latihan/LuasPermukaanKerucut.java
@@ -6,12 +6,14 @@ public class LuasPermukaanKerucut {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.print("Masukan jari-jari: ");
         int jari2 = in.nextInt();
+        System.out.print("Masukan tinggi: ");
         int tinggi = in.nextInt();
 
         double result = luasPermukaanKerucut(tinggi, jari2);
 
-        System.out.printf("%.3f", result);
+        System.out.printf("Luas Permukaan Kerucut adalah : %.3f", result);
     }
 
     public static double luasPermukaanKerucut(int h, int r) {
