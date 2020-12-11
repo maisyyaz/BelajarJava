daf20ca09b741dfdc06f1b12b853960fb44cae4e
maisyyazid <ma39isy@gmail.com>
2020-12-12 06:59:15 +0700

    Upload IMA 14

--- src/com/latihan/TowerOfHanoi.java
+++ src/com/latihan/TowerOfHanoi.java
@@ -9,16 +9,16 @@ public class TowerOfHanoi {
         System.out.print("Masukan angka: ");
         int angka = in.nextInt();
 
-        fungsiTowerOfHanoi(angka, 'A','B','C', "daun atas");
+        fungsiTowerOfHanoi(angka, 'A','B','C');
     }
 
-    static void fungsiTowerOfHanoi(int cakram, char dariBatang, char batangTransisi, char keBatang, String daun) {
+    static void fungsiTowerOfHanoi(int cakram, char dariBatang, char batangTransisi, char keBatang) {
         if (cakram == 1) {
-            System.out.println("Cakram 1 dipindahkan dari " + dariBatang + " ke " + keBatang + " pada " + daun);
+            System.out.println("Cakram 1 dipindahkan dari " + dariBatang + " ke " + keBatang);
         } else {
-            fungsiTowerOfHanoi(cakram - 1, dariBatang, keBatang, batangTransisi, "kiri" );
-            System.out.println("Cakram " + cakram + " dipindahkan dari " + dariBatang + " ke " + keBatang + " pada " + daun);
-            fungsiTowerOfHanoi(cakram - 1, batangTransisi, dariBatang, keBatang, "kanan");
+            fungsiTowerOfHanoi(cakram - 1, dariBatang, keBatang, batangTransisi);
+            System.out.println("Cakram " + cakram + " dipindahkan dari " + dariBatang + " ke " + keBatang);
+            fungsiTowerOfHanoi(cakram - 1, batangTransisi, dariBatang, keBatang);
         }
     }
 }
